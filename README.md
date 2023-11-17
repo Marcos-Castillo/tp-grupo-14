9860157639fdc2ba2560b53c3e257bdd48ce2a2b
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2023-11-17 17:06:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tp-grupo-14
+ trabajo practico final de curso de Spring Boot en codo a codo 
---
+++ src/main/java/com/codoacodo23650/tpgrupo14/TpGrupo14Application.java
@@ -0,0 +1,13 @@
+package com.codoacodo23650.tpgrupo14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGrupo14Application.class, args);
+	}
+
+}
