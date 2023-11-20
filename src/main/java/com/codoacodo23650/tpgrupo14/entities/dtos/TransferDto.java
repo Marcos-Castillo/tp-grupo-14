8c0eb23781d5a4d413c4b6707ff36f4fce5c02e0
cintiacas <cintiscas@hotmail.com>
2023-11-20 17:48:19 -0300

    mas clases transfer

--- src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/TransferDto.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/TransferDto.java
@@ -1,4 +1,26 @@
 package com.codoacodo23650.tpgrupo14.entities.dtos;
 
+import com.codoacodo23650.tpgrupo14.entities.Account;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class TransferDto {
+
+    private Long id;
+
+    private Double amount;
+
+    private Account accountOrigin;
+
+    private Account accountDestination;
+
+    private LocalDateTime date;
+
 }
---
+++ src/main/java/com/codoacodo23650/tpgrupo14/mappers/TransferMapper.java
@@ -0,0 +1,4 @@
+package com.codoacodo23650.tpgrupo14.mappers;
+
+public class TransferMapper {
+}
