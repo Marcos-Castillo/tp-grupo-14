d996c259e5572a083c9ccbd7b4b630c9b4a4ce91
cintiacas <cintiscas@hotmail.com>
2023-11-20 17:48:19 -0300

    mas clases transfer

---
+++ src/main/java/com/codoacodo23650/tpgrupo14/controllers/TransferController.java
@@ -0,0 +1,4 @@
+package com.codoacodo23650.tpgrupo14.controllers;
+
+public class TransferController {
+}
---
+++ src/main/java/com/codoacodo23650/tpgrupo14/repositories/TransferRepository.java
@@ -0,0 +1,7 @@
+package com.codoacodo23650.tpgrupo14.repositories;
+
+import com.codoacodo23650.tpgrupo14.entities.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
