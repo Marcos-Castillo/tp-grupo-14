04cbf2f44edcdc2ccbb231acd2c69da57e92542f
christianlorenzo <christianfagan@hotmail.com>
2023-11-20 19:02:54 -0300

    Arreglo TranferDto

--- src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/TransferDto.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/TransferDto.java
@@ -1,7 +1,6 @@
 package com.codoacodo23650.tpgrupo14.entities.dtos;
 
 import com.codoacodo23650.tpgrupo14.entities.Account;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
