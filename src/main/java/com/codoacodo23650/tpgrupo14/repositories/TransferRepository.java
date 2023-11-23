2b8484daf3213d85001ae352d078965c7eac3dcb
cintiacas <cintiscas@hotmail.com>
2023-11-22 21:01:34 -0300

    anotaciones faltantes

--- src/main/java/com/codoacodo23650/tpgrupo14/repositories/TransferRepository.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/repositories/TransferRepository.java
@@ -2,6 +2,8 @@ package com.codoacodo23650.tpgrupo14.repositories;
 
 import com.codoacodo23650.tpgrupo14.entities.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
 }
