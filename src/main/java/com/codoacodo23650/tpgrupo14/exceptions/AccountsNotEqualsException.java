e093a42f47ca53df7e2ba35964b7120206c0e18d
cintiacas <cintiscas@hotmail.com>
2023-11-30 13:07:12 -0300

    modificaciones varias

---
+++ src/main/java/com/codoacodo23650/tpgrupo14/exceptions/AccountsNotEqualsException.java
@@ -0,0 +1,2 @@
+package com.codoacodo23650.tpgrupo14.exceptions;public class AccountsNotEqualsException {
+}
--- src/main/java/com/codoacodo23650/tpgrupo14/services/TransferService.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/services/TransferService.java
@@ -1,9 +1,9 @@
 package com.codoacodo23650.tpgrupo14.services;
 
-
 import com.codoacodo23650.tpgrupo14.entities.Account;
 import com.codoacodo23650.tpgrupo14.entities.Transfer;
 import com.codoacodo23650.tpgrupo14.entities.dtos.TransferDto;
+import com.codoacodo23650.tpgrupo14.exceptions.*;
 import com.codoacodo23650.tpgrupo14.mappers.TransferMapper;
 import com.codoacodo23650.tpgrupo14.repositories.TransferRepository;
 import com.codoacodo23650.tpgrupo14.repositories.AccountRepository;
@@ -15,11 +15,12 @@ import java.util.stream.Collectors;
 
 @Service
 public class TransferService {
-    private TransferRepository repository;
-    private AccountRepository repo_acc;
+    private final TransferRepository repository;
+    private final AccountRepository repo_acc;
 
-    public TransferService(TransferRepository repository){
+    public TransferService(TransferRepository repository, AccountRepository repo_acc){
         this.repository = repository;
+        this.repo_acc = repo_acc;
     }
 
     public List<TransferDto> getTransfers(){
@@ -29,46 +30,32 @@ public class TransferService {
     }
 
     public TransferDto getTransferById(Long id){
-        Transfer tran = repository.findById(id).get();
-        return TransferMapper.transferToDto(tran);
+        Transfer transfer = repository.findById(id).orElseThrow(() ->
+                new TransferNotFoundException("Transfer not found with id: " + id));
+        return TransferMapper.transferToDto(transfer);
     }
 
     public TransferDto createTransfer(TransferDto dto){
-        //Transfer entity = TransferMapper.dtoToTransfer(dto);
+        TransferDto resp_trans;
+        resp_trans = validateTransfer(dto);
 
-        if (dto.getAccountOrigin() == null){
-            return null;
-        }
-
-        if (dto.getAccountDestination() == null){
-            return null;
-        }
-
-        if (dto.getAccountOrigin() == dto.getAccountDestination()){
-            return null;
-        }
-
-        if (dto.getAmount() == null || dto.getAmount() == 0){
-            return null;
-        }
-
-        if(!repo_acc.existsById(dto.getAccountOrigin()))
+        if (!(resp_trans == null))
         {
-            return null;
-        }
-
-        if(!repo_acc.existsById(dto.getAccountDestination()))
-        {
-            return null;
+            return resp_trans;
         }
 
         Account accountOrigin = repo_acc.findById(dto.getAccountOrigin()).get();
+        Account accountDestination = repo_acc.findById(dto.getAccountDestination()).get();
 
-        if(accountOrigin.getAmount() < dto.getAmount())
-        {
-            return null;
-        }
+        Double res_acc = accountOrigin.getAmount() - dto.getAmount();
+        Double sum_acc = accountDestination.getAmount() + dto.getAmount();
+        accountOrigin.setAmount(res_acc);
+        accountDestination.setAmount(sum_acc);
 
+        repo_acc.save(accountOrigin);
+        repo_acc.save(accountDestination);
+
+        // Setear fecha de transaccion
         dto.setDate(LocalDateTime.now());
 
         Transfer newTransfer = TransferMapper.dtoToTransfer(dto);
@@ -76,71 +63,37 @@ public class TransferService {
     }
 
     public TransferDto updateTransfer(Long id, TransferDto dto) {
-        Boolean ok_validate;
-
         if (repository.existsById(id)){
-            Transfer transferToModify = repository.findById(id).get();
+            TransferDto resp_trans;
+            resp_trans = validateTransfer(dto);
+
+            if (!(resp_trans == null))
+            {
+                return resp_trans;
+            }
 
             Account accountOrigin = repo_acc.findById(dto.getAccountOrigin()).get();
+            Account accountDestination = repo_acc.findById(dto.getAccountDestination()).get();
 
-            if (dto.getAmount() != null){
-                ok_validate = true;
+            Double res_acc = accountOrigin.getAmount() - dto.getAmount();
+            Double sum_acc = accountDestination.getAmount() + dto.getAmount();
+            accountOrigin.setAmount(res_acc);
+            accountDestination.setAmount(sum_acc);
 
-                if (dto.getAmount() == 0){
-                    ok_validate = false;
-                }
+            repo_acc.save(accountOrigin);
+            repo_acc.save(accountDestination);
 
-                if(accountOrigin.getAmount() < dto.getAmount())
-                {
-                    ok_validate = false;
-                }
-
-                if(ok_validate)
-                {
-                    transferToModify.setAmount(dto.getAmount());
-                }
-            }
-
-            if (dto.getAccountOrigin() != null){
-                ok_validate = true;
-
-                if (dto.getAccountOrigin() == dto.getAccountDestination()){
-                    ok_validate = false;
-                }
-
-                if(!repo_acc.existsById(dto.getAccountOrigin()))
-                {
-                    ok_validate = false;
-                }
-
-                if(ok_validate) {
-                    transferToModify.setAccountOrigin(dto.getAccountOrigin());
-                }
-            }
-
-            if (dto.getAccountDestination() != null){
-                ok_validate = true;
-
-                if (dto.getAccountOrigin() == dto.getAccountDestination()) {
-                    ok_validate = false;
-                }
-
-                if(!repo_acc.existsById(dto.getAccountDestination()))
-                {
-                    ok_validate = false;
-                }
-
-                if(ok_validate) {
-                    transferToModify.setAccountDestination(dto.getAccountDestination());
-                }
-            }
-
+            Transfer transferToModify = repository.findById(id).get();
+            transferToModify.setAmount(dto.getAmount());
+            transferToModify.setAccountOrigin(dto.getAccountOrigin());
+            transferToModify.setAccountDestination(dto.getAccountDestination());
             Transfer transferModified = repository.save(transferToModify);
 
             return TransferMapper.transferToDto(transferModified);
-        }
 
-        return null;
+        } else {
+                throw new TransferNotFoundException("Transfer not found with id: " + id);
+        }
     }
 
     public String deleteTransfer(Long id){
@@ -150,5 +103,37 @@ public class TransferService {
         } else {
             return "La transferencia con id: " + id + ", no ha sido eliminada";
         }
+    }
+
+    public TransferDto validateTransfer(TransferDto dto)
+    {
+        if(!repo_acc.existsById(dto.getAccountOrigin()))
+        {
+            throw new AccountNotFoundException("Account not found with id: " + dto.getAccountOrigin());
+        }
+
+        if(!repo_acc.existsById(dto.getAccountDestination()))
+        {
+            throw new AccountNotFoundException("Account not found with id: " + dto.getAccountDestination());
+        }
+
+        if (dto.getAccountOrigin() == dto.getAccountDestination()){
+            //return null;
+            throw new AccountsNotEqualsException("Origin Account is equal to Destination Account.");
+        }
+
+        if (dto.getAmount() == null || dto.getAmount() == 0){
+            throw new AmountZeroException("Amount cannot be empty or zero.");
+        }
+
+        Account accountOrigin = repo_acc.findById(dto.getAccountOrigin()).get();
+        Account accountDestination = repo_acc.findById(dto.getAccountDestination()).get();
+
+        if(accountOrigin.getAmount() < dto.getAmount())
+        {
+            throw new InsufficientFoundsException("Insufficient funds in the account with id: " + dto.getAccountOrigin());
+        }
+
+        return null;
     }
 }
