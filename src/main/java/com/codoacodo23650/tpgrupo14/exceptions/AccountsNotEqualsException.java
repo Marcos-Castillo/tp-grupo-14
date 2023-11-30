76a62df2775ef5535d431fc0d2400510ec9f82d2
cintiacas <cintiscas@hotmail.com>
2023-11-30 20:40:13 -0300

    exceptions

--- src/main/java/com/codoacodo23650/tpgrupo14/exceptions/AccountsNotEqualsException.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/exceptions/AccountsNotEqualsException.java
@@ -1,2 +1,7 @@
-package com.codoacodo23650.tpgrupo14.exceptions;public class AccountsNotEqualsException {
+package com.codoacodo23650.tpgrupo14.exceptions;
+
+public class AccountsNotEqualsException extends RuntimeException{
+    public AccountsNotEqualsException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/codoacodo23650/tpgrupo14/exceptions/AmountZeroException.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/exceptions/AmountZeroException.java
@@ -1,2 +1,7 @@
-package com.codoacodo23650.tpgrupo14.exceptions;public class AmountZeroException {
+package com.codoacodo23650.tpgrupo14.exceptions;
+
+public class AmountZeroException extends RuntimeException{
+    public AmountZeroException(String message) {
+        super(message);
+    }
 }
