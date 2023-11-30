7113485dd930120cbb52ed324438dff25c70434b
cintiacas <cintiscas@hotmail.com>
2023-11-30 20:37:26 -0300

    exeption

---
+++ src/main/java/com/codoacodo23650/tpgrupo14/entities/Loan.java
@@ -0,0 +1,2 @@
+package com.codoacodo23650.tpgrupo14.entities;public class Loan {
+}
--- src/main/java/com/codoacodo23650/tpgrupo14/exceptions/TransferNotFoundException.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/exceptions/TransferNotFoundException.java
@@ -1,2 +1,7 @@
-package com.codoacodo23650.tpgrupo14.exceptions;public class TransferNotFoundException {
+package com.codoacodo23650.tpgrupo14.exceptions;
+
+public class TransferNotFoundException extends RuntimeException {
+    public TransferNotFoundException(String message) {
+        super(message);
+    }
 }
