ab41b52c8226ce0bc5a97ce513569eeb722ecfde
cintiacas <cintiscas@hotmail.com>
2023-11-30 20:42:42 -0300

    class loan

--- src/main/java/com/codoacodo23650/tpgrupo14/entities/Loan.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/entities/Loan.java
@@ -1,2 +1,37 @@
-package com.codoacodo23650.tpgrupo14.entities;public class Loan {
+package com.codoacodo23650.tpgrupo14.entities;
+
+import com.codoacodo23650.tpgrupo14.entities.enums.StatusLoan;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table(name = "préstamos")
+@Getter
+@Setter
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "importe")
+    private Double amount;
+
+    @Column(name = "interes")
+    private Double interest;
+
+    @Column(name = "cuotas")
+    private Long dues;
+
+    @Column(name = "fecha_prestamo")
+    private LocalDateTime date;
+
+    @Column(name = "estado")
+    private StatusLoan status;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
 }
--- src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/LoanDto.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/entities/dtos/LoanDto.java
@@ -1,2 +1,20 @@
-package com.codoacodo23650.tpgrupo14.entities.dtos;public class LoanDto {
+package com.codoacodo23650.tpgrupo14.entities.dtos;
+
+import com.codoacodo23650.tpgrupo14.entities.Account;
+import com.codoacodo23650.tpgrupo14.entities.enums.StatusLoan;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class LoanDto {
+    private Long id;
+    private Double amount;
+    private Double interest;
+    private Long dues;
+    private LocalDateTime date;
+    private StatusLoan status;
+    private Account account;
 }
