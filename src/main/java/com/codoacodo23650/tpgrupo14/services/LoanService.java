ea8760d01ac833ca903e3bd7567603b6cfb8ac11
christianlorenzo <christianfagan@hotmail.com>
2023-12-05 14:10:21 -0300

    Loan Status validation at payment

--- src/main/java/com/codoacodo23650/tpgrupo14/services/LoanService.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/services/LoanService.java
@@ -5,6 +5,7 @@ import com.codoacodo23650.tpgrupo14.entities.Loan;
 import com.codoacodo23650.tpgrupo14.entities.dtos.LoanDto;
 import com.codoacodo23650.tpgrupo14.entities.enums.StatusLoan;
 import com.codoacodo23650.tpgrupo14.exceptions.LoanDueException;
+import com.codoacodo23650.tpgrupo14.exceptions.StatusInvalidException;
 import com.codoacodo23650.tpgrupo14.mappers.LoanMapper;
 import com.codoacodo23650.tpgrupo14.mappers.TransferMapper;
 import com.codoacodo23650.tpgrupo14.repositories.AccountRepository;
@@ -78,22 +79,25 @@ public class LoanService {
             if (accountRepository.existsById(accountId))
             {
                 Loan existingLoan = repository.findById(loanId).get();
-                Account existingAccount = accountRepository.findById(accountId).get();
-                if (amountToPay>0 && amountToPay <=existingAccount.getAmount())
-                {
-                    Double dueAmount = (existingLoan.getAmount() * ( 1 + (existingLoan.getInterest()/100))) / existingLoan.getDues();
-                    if(amountToPay >= dueAmount) {
-                        existingLoan.setAmount(existingLoan.getAmount() - (amountToPay / (1 + (existingLoan.getInterest()/100))));
-                        existingLoan.setDues(existingLoan.getDues() - 1);
-                        if (existingLoan.getDues() == 0 || !(existingLoan.getAmount()>0)) {
-                            existingLoan.setStatus(StatusLoan.FINISHED);
+                if (existingLoan.getStatus() == StatusLoan.FINISHED || existingLoan.getStatus() == StatusLoan.REFUSED) {
+                    throw new StatusInvalidException("Prestamo con estado FINISHED o REFUSED");
+                } else {
+                    Account existingAccount = accountRepository.findById(accountId).get();
+                    if (amountToPay > 0 && amountToPay <= existingAccount.getAmount()) {
+                        Double dueAmount = (existingLoan.getAmount() * (1 + (existingLoan.getInterest() / 100))) / existingLoan.getDues();
+                        if (amountToPay >= dueAmount) {
+                            existingLoan.setAmount(existingLoan.getAmount() - (amountToPay / (1 + (existingLoan.getInterest() / 100))));
+                            existingLoan.setDues(existingLoan.getDues() - 1);
+                            if (existingLoan.getDues() == 0 || !(existingLoan.getAmount() > 0)) {
+                                existingLoan.setStatus(StatusLoan.FINISHED);
+                            }
+                            existingAccount.setAmount(existingAccount.getAmount() - amountToPay);
+                            accountRepository.save(existingAccount);
+                            repository.save(existingLoan);
+                            return "El préstamo con id: " + loanId + " ha sido abonado " + amountToPay + " desde cuenta " + accountId;
+                        } else {
+                            throw new LoanDueException("El valor minimo a abonar debe ser mayor o igual al valor de cuota ($" + dueAmount + ").");
                         }
-                        existingAccount.setAmount(existingAccount.getAmount() - amountToPay);
-                        accountRepository.save(existingAccount);
-                        repository.save(existingLoan);
-                        return "El préstamo con id: " + loanId + " ha sido abonado " + amountToPay + " desde cuenta " + accountId;
-                    } else {
-                        throw new LoanDueException("El valor minimo a abonar debe ser mayor o igual al valor de cuota ($" + dueAmount + ").");
                     }
                 }
             }
