9e4a02e3df95ab18e6b7356c3c874a9c82633159
javpintos <jpintos234@gmail.com>
2023-12-06 15:57:02 -0300

    UserNotFoundException and UserBadRequestException ready

--- src/main/java/com/codoacodo23650/tpgrupo14/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/codoacodo23650/tpgrupo14/exceptions/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.codoacodo23650.tpgrupo14.exceptions.handler;
 
 import com.codoacodo23650.tpgrupo14.exceptions.dtos.ErrorMessageDto;
+import com.codoacodo23650.tpgrupo14.exceptions.exceptionKinds.UserBadRequestException;
+import com.codoacodo23650.tpgrupo14.exceptions.exceptionKinds.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +11,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class  GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
     public ResponseEntity<ErrorMessageDto> defaultErrorHandler(HttpServletRequest req, Exception e){
         return new ResponseEntity<>(new ErrorMessageDto(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorMessageDto> notFoundHandler(HttpServletRequest req, Exception e){
+        return new ResponseEntity<>(new ErrorMessageDto(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserBadRequestException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorMessageDto> badRequestHandler(HttpServletRequest req, Exception e){
+        return new ResponseEntity<>(new ErrorMessageDto(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
 }
